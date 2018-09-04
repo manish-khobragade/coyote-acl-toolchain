738d1f65609325c09d4704d8b781b6883d7179ba
Khobragade <Manish.Khobragade@emtecinc.com>
2018-09-04 17:09:07 +0530

    Changed the network

--- contracts/coyote-acl-network/lib/logic.js
+++ contracts/coyote-acl-network/lib/logic.js
@@ -18,6 +18,12 @@ String.prototype.toDateFromDatetime = function () {
     return new Date(parts[0], parts[1] - 1, parts[2], parts[3], parts[4], parts[5]);
 };
 
+function diffMinutes(dt2, dt1) {
+    var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+    diff /= 60;
+    return Math.abs(Math.round(diff));
+}
+
 /**
  * A shipment has been received by the customer
  * @param {org.coyote.playground.blockchain.demo.ShipmentReceived} shipmentReceived - the ShipmentReceived transaction
@@ -33,14 +39,15 @@ function shipmentDelivered(shipmentReceived) {
     var deliveryTimeActual = shipmentReceived.actualDeliveredTime;
     // set the status of the shipment
     shipment.status = 'DELIVERED';
+    var message = 'Shipment has arrived at the destination';
 
 
     // calculate penalty for temperature violation
     if (shipment.temperatureReadings) {
-        var minimumTempViolationCount = shipment.temperatureReadings.filter(function (reading) { return reading < contract.minTemperature; }).length;
-        var maxTempViolationCount = shipment.temperatureReadings.filter(function (reading) { return reading > contract.maxTemperature; }).length;
-        console.log('Min Temp Count' + minimumTempViolationCount);
-        console.log('Max Temp Count' + maxTempViolationCount);
+        var minimumTempViolationCount = shipment.temperatureReadings.filter(function (reading) { return reading.centigrade < contract.minTemperature; }).length;
+        var maxTempViolationCount = shipment.temperatureReadings.filter(function (reading) { return reading.centigrade > contract.maxTemperature; }).length;
+        console.log('Min Temp Count : ' + minimumTempViolationCount);
+        console.log('Max Temp Count : ' + maxTempViolationCount);
         if (minimumTempViolationCount > 0) {
             penalty += (minimumTempViolationCount * contract.minTempViolationPenalty);
             console.log('Min Penalty : ' + minimumTempViolationCount * contract.minTempViolationPenalty);
@@ -68,9 +75,11 @@ function shipmentDelivered(shipmentReceived) {
         shipment.loadStops[index].actualTime = deliveryTimeActual;
         if (appointmentTimeDelivery < actualTimeDelivery) {
             penalty += contract.deliveryLateFee;
+            var timeDiffInMinutes = diffMinutes(actualTimeDelivery, appointmentTimeDelivery);
+            message = 'Carrier checked in ' + timeDiffInMinutes + ' minutes past scheduled appointment time of ' + appointmentTimeDelivery;
         }
     }
-
+    console.log('Total Penalty : ' + penalty);
     payOut -= penalty;
     contract.customer.accountBalance -= payOut;
     contract.broker.accountBalance += ((payOut * contract.brokerMargin) / 100);
@@ -81,8 +90,12 @@ function shipmentDelivered(shipmentReceived) {
     shipmentArrived.shipment = shipment;
     shipmentArrived.shipmentAmount = shipmentAmount;
     shipmentArrived.penalty = penalty;
-    var message = 'Shipment has arrived at the destination';
     shipmentArrived.message = message;
+
+    //Shipment
+    shipment.totalAmount = shipmentAmount;
+    shipment.totalPenalty = penalty;
+
     emit(shipmentArrived);
 
     return getParticipantRegistry('org.coyote.playground.blockchain.demo.Customer')
@@ -137,12 +150,21 @@ function temperatureReading(temperatureReading) {
 
     if (temperatureReading.centigrade < contract.minTemperature ||
         temperatureReading.centigrade > contract.maxTemperature) {
-        var violationType = temperatureReading.centigrade < contract.minTemperature ? 'Minimum Temperature Violation' : 'Maximum Temperature Violation';
+        var violationType = '';
+        var message = '';
+        if (temperatureReading.centigrade < contract.minTemperature) {
+            violationType = 'Temperature Below Threshold';
+            message = 'Shipment temperature fell below designated threshold of ' + contract.minTemperature;
+        } else {
+            violationType = 'Temperature Above Threshold';
+            message = 'Shipment temperature rose above designated threshold of ' + contract.maxTemperature;
+        }
+
         var temperatureEvent = factory.newEvent(NS, 'TemperatureThresholdEvent');
         temperatureEvent.shipment = shipment;
         temperatureEvent.temperature = temperatureReading.centigrade;
         temperatureEvent.temperatureViolationType = violationType;
-        temperatureEvent.message = 'Temperature threshold violated! Emitting TemperatureEvent for shipment: ' + shipment.$identifier;
+        temperatureEvent.message = message;
         emit(temperatureEvent);
     }
 
@@ -158,11 +180,9 @@ function temperatureReading(temperatureReading) {
  * @transaction
  */
 function gpsReading(gpsReading) {
-
     var factory = getFactory();
     var NS = "org.coyote.playground.blockchain.demo";
     var shipment = gpsReading.shipment;
-
 
     if (shipment.gpsReadings) {
         shipment.gpsReadings.push(gpsReading);
@@ -196,14 +216,13 @@ function gpsReading(gpsReading) {
 function shipmentAccepted(shipmentAccepted) {
     var shipment = shipmentAccepted.shipment;
     var NS = 'org.coyote.playground.blockchain.demo';
-    
     shipment.status = 'ACCEPTED';
+    console.log('Shipment current ' + shipment);
     var shipmentRegistry = getAssetRegistry(NS + '.Shipment')
         .then(function (shipmentRegistry) {
             // add the accepted state to the shipment
             return shipmentRegistry.update(shipment);
         });
-
 }
 
 
@@ -213,6 +232,7 @@ function shipmentAccepted(shipmentAccepted) {
  * @transaction
  */
 function shipmentPickedUp(shipmentPicked) {
+    var factory = getFactory();
     var shipment = shipmentPicked.shipment;
     var pickUpTime = shipmentPicked.actualPickupTime
     var NS = 'org.coyote.playground.blockchain.demo';
@@ -220,8 +240,22 @@ function shipmentPickedUp(shipmentPicked) {
     if (shipment.loadStops) {
         var loadStopPickup = shipment.loadStops.filter(function (ls) { return ls.stopType === 'PICKUP' })[0];
         if (loadStopPickup != null) {
+
             var index = shipment.loadStops.indexOf(loadStopPickup);
             shipment.loadStops[index].actualTime = pickUpTime;
+
+            var appointmentTimePickup = shipment.loadStops[index].appointmentTime.toDateFromDatetime();
+            var actualTimePickup = shipment.loadStops[index].actualTime.toDateFromDatetime();
+
+            if (appointmentTimePickup < actualTimePickup) {
+                var shipmentLatePickedUp = factory.newEvent(NS, 'ShipmentLatePickup');
+                shipmentLatePickedUp.shipment = shipment;
+                var timeDiffInMinutes = diffMinutes(actualTimePickup, appointmentTimePickup);
+                var message = 'Carrier checked in ' + timeDiffInMinutes + ' minutes past scheduled appointment time of ' + appointmentTimePickup;
+                shipmentLatePickedUp.message = message;
+                emit(shipmentLatePickedUp);
+            }
+
             var shipmentRegistry = getAssetRegistry(NS + '.Shipment')
                 .then(function (shipmentRegistry) {
                     // add the accepted state to the shipment
